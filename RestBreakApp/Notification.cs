d3eede3bd08c6a4b63fead2b6c80b22e990246a1
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2021-12-31 12:31:46 -0800

    added icon;

--- RestBreakApp/Notification.cs
+++ RestBreakApp/Notification.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace RestBreakService
 {
     public class Notification
     {
+        private readonly string projectName;
+        private int Id;
+        public Notification()
+        {
+            projectName = Assembly.GetCallingAssembly().GetName().Name;
+            Id = 0;
+        }
         public void ShowNotification()
         {
             new ToastContentBuilder()
                 .AddArgument("action", "showNotification")
-                .AddArgument("notificationId", 1)
+                .AddArgument("notificationId", Id)
+                .SetToastScenario(ToastScenario.Reminder)                
                 .AddText("Time to take a break")
-                .AddText("Take a break dammit!")
+                .AddText("Take a break dammit!")                
                 .Show();
         }
     }
