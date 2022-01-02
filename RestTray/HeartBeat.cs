9ae1026d6b22a74b78d4e960063b273815bc117a
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2022-01-01 23:19:06 -0800

    removed service project;

---
+++ RestTray/HeartBeat.cs
@@ -0,0 +1,37 @@
+﻿using System.Timers;
+
+namespace RestTray
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+        private readonly Notification _notification;
+#if DEBUG
+        private const int DURATION = 10000;
+#else
+        private const int DURATION = 20 * 60000;
+#endif
+
+        public HeartBeat()
+        {
+            _notification = new Notification();
+            _timer = new Timer(DURATION) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            _notification.ShowNotification();
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
