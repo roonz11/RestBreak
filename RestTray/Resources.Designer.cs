8ee1b2427e8b2e98c848078731bd6bda5089238d
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2022-01-02 16:50:10 -0800

    updated icons;

--- RestTray/Resources.Designer.cs
+++ RestTray/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace RestTray {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon MyIcon {
-            get {
-                object obj = ResourceManager.GetObject("MyIcon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
