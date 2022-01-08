3e939f707b5596d0e1fd0b0016b620760578c5eb
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2022-01-08 12:38:53 -0800

    added SessionRepo

---
+++ RestTray/Repositories/ISessionRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestTray.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestTray.Repositories
+{
+    public interface ISessionRepository
+    {
+        Task<int> AddSessionAsync(Session session);
+        Task<IEnumerable<Session>> GetSessionsAsync(int dateFilter);
+    }
+}
---
+++ RestTray/Repositories/SessionRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestTray.Data;
+using RestTray.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestTray.Repositories
+{
+    public class SessionRepository : ISessionRepository
+    {
+        private readonly RestBreakContext _dbContext;
+
+        public SessionRepository(RestBreakContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<int> AddSessionAsync(Session session)
+        {
+            _dbContext.Add(session);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Session>> GetSessionsAsync(int dateFilter = 0)
+        {
+            return await _dbContext.Session
+                .Where(x => dateFilter > 0 ? x.Date >= DateTime.UtcNow.AddDays(-dateFilter) : true)
+                .ToListAsync(); ;
+        }
+    }
+}
