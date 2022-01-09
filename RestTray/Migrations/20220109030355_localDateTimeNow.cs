729b5b1829b263dd8cd2340fb16a1bd66ca8ea15
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2022-01-08 19:34:57 -0800

    updated to DateTime.Now and some colors;

---
+++ RestTray/Migrations/20220109030355_localDateTimeNow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestTray.Migrations
+{
+    public partial class localDateTimeNow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Session",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Session",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+    }
+}
